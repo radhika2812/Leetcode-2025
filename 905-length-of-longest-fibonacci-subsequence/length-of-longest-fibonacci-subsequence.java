7e76b887bfbd5f9f723ade7afb95d82525ea80ec
radhika2812 <128672913+radhika2812@users.noreply.github.com>
2025-02-27 17:39:26 +0530

    Time: 169 ms (66.27%) | Memory: 56.5 MB (16.57%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,25 +1,25 @@
-
 class Solution {
-  public int lenLongestFibSubseq(int[] arr) {
-    final int n = arr.length;
-    int ans = 0;
-    int[][] dp = new int[n][n];
-    Arrays.stream(dp).forEach(A -> Arrays.fill(A, 2));
-    Map<Integer, Integer> numToIndex = new HashMap<>();
-
-    for (int i = 0; i < n; ++i)
-      numToIndex.put(arr[i], i);
-
-    for (int j = 0; j < n; ++j)
-      for (int k = j + 1; k < n; ++k) {
-        final int ai = arr[k] - arr[j];
-        if (ai < arr[j] && numToIndex.containsKey(ai)) {
-          final int i = numToIndex.get(ai);
-          dp[j][k] = dp[i][j] + 1;
-          ans = Math.max(ans, dp[j][k]);
+    public int lenLongestFibSubseq(int[] arr) {
+        int n = arr.length;
+        int[][] f = new int[n][n];
+        Map<Integer, Integer> d = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            d.put(arr[i], i);
+            for (int j = 0; j < i; ++j) {
+                f[i][j] = 2;
+            }
         }
-      }
-
-    return ans;
-  }
+        int ans = 0;
+        for (int i = 2; i < n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                int t = arr[i] - arr[j];
+                Integer k = d.get(t);
+                if (k != null && k < j) {
+                    f[i][j] = Math.max(f[i][j], f[j][k] + 1);
+                    ans = Math.max(ans, f[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
 }
