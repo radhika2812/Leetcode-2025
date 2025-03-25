3fd72c15fe31d1d57645bba3889ba77ceeba124e
radhika2812 <128672913+radhika2812@users.noreply.github.com>
2025-03-25 15:39:07 +0530

    Time: 1711 ms (27.08%) | Memory: 18.6 MB (72.18%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,6 +5,6 @@ class Solution:
         for i in range(1, len(nums)):  # ✅ i starts from 1
             for j in range(0, i):  # ✅ j starts from 0 and is always < i
                 if nums[i] + nums[j] == target:
-                    return [j, i]  # ✅ Return directly when found
+                    return [i,j]  # ✅ Return directly when found
         return []  # ✅ Return an empty list if no pair is found
 
